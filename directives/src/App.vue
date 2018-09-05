cb9c6c221568c0a50659e3fb6dea41e849c40ae2
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-09-05 09:30:53 -0700

    custom directives

---
+++ directives/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-center">
+                <h1>Directives</h1>
+                <p v-html="'<h6>Random h3 Using html directive</h6>'"></p>
+            </div>
+        </div>
+        <hr>
+         <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-center">
+                <h1>Custom Directives</h1>
+                <p v-highlight:background.delayed="'orangered'">Color This</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
