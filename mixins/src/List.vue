3cacae06caac66fcf307a6a1521e80671b7c56e8
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-09-25 19:41:39 -0700

    vue stuff

--- mixins/src/List.vue
+++ mixins/src/List.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Filters & Mixins</h1>
     <input type="text" v-model="filteredText">
+    <button @click="fruits.push('berries')">Add New Item</button>
     <ul>
       <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
     </ul>
