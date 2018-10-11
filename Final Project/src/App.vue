28c71cab038081e9eeb8dec0edc63885f25a7536
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-10-10 22:00:28 -0700

    routes set up

--- Final Project/src/App.vue
+++ Final Project/src/App.vue
@@ -1,16 +1,22 @@
 <template>
-    <div class="container">
-
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-12">
+        <router-view></router-view>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
-    export default {
-
-    }
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    AppHeader: Header
+  }
+};
 </script>
 
 <style>
-
 </style>
